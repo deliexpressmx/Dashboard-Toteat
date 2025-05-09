bd6f68ef99584a5aaf63efcc5484362fe246b4a5
deliexpressmx <206948356+deliexpressmx@users.noreply.github.com>
2025-05-09 15:48:50 -0600

    Add files via upload

---
+++ home.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.set_page_config(page_title="Dashboard Central", layout="wide")
+st.title("🧠 Dashboard General")
+
+st.markdown("""
+Bienvenido al dashboard central. Usa el menú lateral para navegar entre:
+- 📊 Ventas
+- 🚦 Tráfico
+- 😠 Quejas
+- 📥 Subida de datos
+""")
